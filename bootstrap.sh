5e3c19a5191dd8a810e4dbfd0a86257ae4ee3beb
Brandon Burnett <gentoolicious@gmail.com>
2014-01-08 12:59:18 -0800

    Updated bootstrap script to dynamically link any file that begins with .

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,34 +5,49 @@
 # 2. Install oh-my-zsh from GitHub
 # 3. Link appropriate dot-files
 #
-DEV_DIR = "${HOME}/development"
+DEV_DIR="${HOME}/development"
 
-zsh --version 2> /dev/null
+zsh --version 2>&1 /dev/null
 if [ $? -eq 127 ]; then
   echo "Install ZSH!~"
   exit 1
 fi
 
-git --version 2> /dev/null
+git --version 2>&1 /dev/null
 if [ $? -eq 127 ]; then
   echo "Install Git!~"
   exit 1
 fi
 
 # Install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ${DEV_DIR}/.oh-my-zsh
+if [ ! -e ${DEV_DIR}/.oh-my-zsh ]; then
+  echo "Installing oh-my-zsh..."
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ${DEV_DIR}/.oh-my-zsh
+  if [ $? -eq 128 ]; then
+    echo "Problem installing oh-my-zsh. Do you need to set a proxy?"
+    exit 1
+  fi
+fi
 
 # Link dot-files
-ln -s .aliases ~/
-ln -s .config ~/
-ln -s .gemrc ~/
-ln -s .git ~/
-ln -s .linuxrc ~/
-ln -s .macrc ~/
-ln -s .nikerc ~/
-ln -s .profile ~/
-ln -s .tmux.conf ~/
-ln -s .zshrc ~/
+for f in .*
+do
+  if [ $f != "." ] && [ $f != ".." ]; then
+    FILE=${HOME}/${f}
+    if [ -e $FILE ]; then
+      echo "Replace $FILE? [y/N]"
+      read answer
+      if [ $answer == "y" ]; then
+        echo "Replacing $f..."
+        mv $FILE $FILE.old
+        ln -s $f ~/
+      fi
+    else
+      echo "Linking $f..."
+      ln -s $f ~/
+    fi
+  fi
+done
 
 # Change shell to zsh
 chsh -s /bin/zsh
