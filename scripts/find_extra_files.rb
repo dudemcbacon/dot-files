605d9e778186bfe2bfa1994b445eb9ba896dc412
Brandon Burnett <gentoolicious@gmail.com>
2025-03-25 20:02:02 -0700

    latest from mac

--- scripts/find_extra_files.rb
+++ scripts/find_extra_files.rb
@@ -12,7 +12,7 @@ def check_directory_files(base_dir, num_dirs = nil)
     path = File.join(base_dir, entry)
     files = Dir.entries(path).select do |file| 
       file_path = File.join(path, file)
-      return false unless File.file?(file_path)
+      next false unless File.file?(file_path)
 
       allowed_extensions = ['.mkv', '.en.srt']
       if file.end_with?(*allowed_extensions)
@@ -20,9 +20,10 @@ def check_directory_files(base_dir, num_dirs = nil)
         extension = file.end_with?('.en.srt') ? '.en.srt' : File.extname(file)
         base_name = File.basename(file, extension)
         
-        # Compare normalized names (without hyphens)
-        base_name.gsub('-', '') == entry.gsub('-', '')
+        # Show files that don't match the directory name
+        base_name.gsub('-', '') != entry.gsub('-', '')
       else
+        # Show all other files
         true
       end
     end
