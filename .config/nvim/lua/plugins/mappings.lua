4b9e5c796925b745365b98010ddf8a26a207557c
Brandon Burnett <gentoolicious@gmail.com>
2025-07-31 10:30:44 -0700

    updated nvim configs

---
+++ .config/nvim/lua/plugins/mappings.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          ["<A-o>"] = { ":lua require'jdtls'.organize_imports()<CR>", desc = "Organize Java Imports" },
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          K = {
+            function() vim.lsp.buf.hover() end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function() vim.lsp.buf.declaration() end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
+}
